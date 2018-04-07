8ad2b5fee4197e20f76760db49fa0527e9c45f17
Sopan Shewale <sopan.shewale@gmail.com>
2018-04-07 11:10:11 +0530

    1: Batch four

---
+++ batch_four/session-1/comments.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+#this is a comment in Python
+
+print ("Hello World") #This is also a comment in Python
+
+""" This is an example of a multiline
+comment that spans multiple lines
+...
+"""
+
+print ("Let me try triple quotes")
+'''
+I am also comment
+in muliple lines
+
+'''
+
