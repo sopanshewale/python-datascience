efbcc6d551a328b10736a57e1dae2f8ef9060db3
Sopan Shewale <sopan.shewale@gmail.com>
2018-04-08 19:44:39 +0530

    Adding PDF for session-2

---
+++ batch_four/session-2/lab_one.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+################# lab_one.py ##############
+
+
+  '' The script is suppose to find mean of the ten numbers ''
+
+
+total_numbers = 10
+n_one    = 10
+n_two    = 11
+n_three  = 12
+n_four   = 13
+n_five   = 14
+n_six    = 15
+ n_seven  = 16
+ n_eight  = 17
+ n_nine   = 18
+ n_ten    = 19
+
+sum = 
+
+mean = sum /  
+
+print mean
+
+# BMI  = bodyweight in kg / (height in meter) ^ 2
+bodyweight = 70 
+height = 1.75 
+bmi = ( ) / (  ) ( ) 
+
+if bmi < 18 : 
+    print ("Oh - welcome my friend. Congratulations, you have six packs") 
+else: 
+         print ("Hello - you need to worry about your fitness) 
